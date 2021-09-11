7fd740d5d7d40db4d393fec612bc589498af22a3
Pierre KANOHA <pierre.kanoha@gmail.com>
2021-09-11 20:41:16 +0200

    refactor page base, getter and constructor

--- features/pageobjects/register.page.ts
+++ features/pageobjects/register.page.ts
@@ -1,18 +1,22 @@
 import Page from "./page";
 
 class RegisterPage extends Page {
-    get header() { return $("//h1"); }
-    get fname() { return $("[name=firstname]"); }
-    get lname() { return $("[name=lastname]"); }
+    private get header() { return $("//h1"); }
+    private get fname() { return $("[name=firstname]"); }
+    private get lname() { return $("[name=lastname]"); }
 
-    get gender_radio() { return $$("[name=sex]"); }
-    get experience_radio() { return $$("[name=exp]"); }
-    get favchai_checkbox() { return $$("[name*=Tea]"); }
-    get reason_checkbox() { return $$("[name=tool]"); }
+    private get gender_radio() { return $$("[name=sex]"); }
+    private get experience_radio() { return $$("[name=exp]"); }
+    private get favchai_checkbox() { return $$("[name*=Tea]"); }
+    private get reason_checkbox() { return $$("[name=tool]"); }
 
-    get continent_dropdown() { return $("#continents"); }
-    get selCommands_multiselect() { return $("#selenium_commands"); }
-    get submit_btn() { return $("#submit"); }
+    private get continent_dropdown() { return $("#continents"); }
+    private get selCommands_multiselect() { return $("#selenium_commands"); }
+    private get submit_btn() { return $("#submit"); }
+
+    getHeader() {
+        return this.header;
+    }
 
     async selectDropdown(element: WebdriverIO.ElementArray, value: string) {
         for (let i = 0; i < element.length; i++) {
@@ -21,9 +25,46 @@ class RegisterPage extends Page {
                 await (element[i]).click();
                 break;
             }
+        }
+
     }
-    
+
+    async enterFirstName(firstname: string) {
+        await (await this.fname).setValue(firstname)
     }
+
+    async enterLastName(lastname: string) {
+        await (await this.lname).setValue(lastname)
+    }
+
+    async selectGender(gender: string) {
+        this.selectDropdown(await this.gender_radio, gender);
+    }
+
+    async selectExperience(years: string) {
+        this.selectDropdown(await this.experience_radio, years);
+    }
+
+    async selectFavourite(chaitype: string) {
+        this.selectDropdown(await this.favchai_checkbox, chaitype);
+    }
+
+    async selectReason(reason: string) {
+        this.selectDropdown(await this.reason_checkbox, reason);
+    }
+
+    async selectContinent(continent: string) {
+        await (await this.continent_dropdown).selectByVisibleText(continent);
+    }
+
+    async selectSeleniumCommand(command: string) {
+        await (await this.selCommands_multiselect).selectByVisibleText(command);
+    }
+
+    async clickOnSubmitBtn() {
+        await (await this.submit_btn).click();
+    }
+
 }
 
 export default new RegisterPage();
--- features/step-definitions/register.steps.ts
+++ features/step-definitions/register.steps.ts
@@ -1,39 +1,34 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import registerPage from "../pageobjects/register.page"
 
-Given(/^I am on the page \"([^\"]*)\"$/,async function (appurl) {
+Given(/^I am on the page \"([^\"]*)\"$/, async function (appurl) {
     await browser.url(appurl);
     await browser.maximizeWindow();
 });
 
-Then(/^I validate the page header \"([^\"]*)\"$/,async function (headertext) {
-    expect(registerPage.header).toHaveText(headertext);
+Then(/^I validate the page header \"([^\"]*)\"$/, async function (headertext) {
+    expect(registerPage.getHeader()).toHaveText(headertext);
 });
 
-When(/^I enter firstname (.+) and lastname (.+)$/,async function (fname, lname) {
-    await (registerPage.fname).setValue(fname);
-    await (registerPage.lname).setValue(lname);
-
-});
-
-Then(/^I select gender (.+) years (.+) favorite chai (.+) and reason (.+)$/,async function (gender: string, yrs: string, favchai: string, reason: string) {
-    const gender_radio = await $$("[name=sex]");
-    const experience_radio = await $$("[name=exp]");
-    const favchai_checkbox = await $$("[name*=Tea]");
-    const reason_checkbox = await $$("[name=tool]");
-
-    registerPage.selectDropdown(await registerPage.gender_radio, gender);
-    registerPage.selectDropdown(await registerPage.experience_radio, yrs);
-    registerPage.selectDropdown(await registerPage.favchai_checkbox, favchai);
-    registerPage.selectDropdown(await registerPage.reason_checkbox, reason);
+When(/^I enter firstname (.+) and lastname (.+)$/, async function (fname, lname) {
+    await registerPage.enterFirstName(fname)
+    await registerPage.enterLastName(lname)
 
 });
 
-When(/^I select continent (.+) and commands (.+)$/,async function (continent, command) {
-    await (await registerPage.continent_dropdown).selectByVisibleText(continent);
-    await (await registerPage.selCommands_multiselect).selectByVisibleText(command);
+Then(/^I select gender (.+) years (.+) favorite chai (.+) and reason (.+)$/, async function (gender: string, yrs: string, favchai: string, reason: string) {
+    await registerPage.selectGender(gender);
+    await registerPage.selectExperience(yrs);
+    await registerPage.selectFavourite(favchai);
+    await registerPage.selectReason(reason);
+
 });
 
-When(/^I click on submit button$/,async function () {
-    await (await registerPage.submit_btn).click();
+When(/^I select continent (.+) and commands (.+)$/, async function (continent, command) {
+    await registerPage.selectContinent(continent);
+    await registerPage.selectSeleniumCommand(command);
+});
+
+When(/^I click on submit button$/, async function () {
+    await registerPage.clickOnSubmitBtn();
 });
